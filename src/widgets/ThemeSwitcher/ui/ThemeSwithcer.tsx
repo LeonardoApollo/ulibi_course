1ac17ca7e82a9fda323cad3f22a769c20a0526f0
Mikhail Tyapkov <redcrownmailbox@gmail.com>
2023-05-20 00:35:17 +0300

    module2_lesson3

---
+++ src/widgets/ThemeSwitcher/ui/ThemeSwithcer.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react'
+import { classNames } from 'shared/libs/classNames/classNames';
+import cls from './ThemeSwithcer.module.scss'
+import { Theme, useTheme } from 'app/providers/ThemeProvider'
+import { ReactComponent as LightIcon }from 'shared/assests/icons/theme-light.svg'
+import { ReactComponent as DarkIcon }from 'shared/assests/icons/theme-dark.svg'
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
+
+interface ThemeSwitcherProps {
+    className?: string;
+}
+
+export const ThemeSwithcer: FC<ThemeSwitcherProps> = ({ className }) => {
+    const {theme, toggleTheme} = useTheme();
+    
+    return (
+        
+        <Button
+            theme={ThemeButton.CLEAR} 
+            className={classNames(cls.ThemeSwitcher, {}, [className])} 
+            onClick={toggleTheme}
+        >
+            {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
+        </Button>
+    )
+}
