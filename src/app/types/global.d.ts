d0976d879acce4cd046be3bbb85283fe5d9ffa3d
Mikhail Tyapkov <redcrownmailbox@gmail.com>
2023-05-23 00:12:09 +0300

    module2_lesson8

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,16 +1,16 @@
 declare module '*.module.scss';
 
-declare module "*.png";
-declare module "*.jpg";
-declare module "*.jpeg";
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
 declare module '*.svg?inline' {
     const content: unknown;
     export default content;
 }
-  
+
 declare module '*.svg' {
     import React = require('react');
-  
+
     export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
     const src: string;
     export default src;
--- src/shared/libs/classNames/classNames.ts
+++ src/shared/libs/classNames/classNames.ts
@@ -1,4 +1,3 @@
-
 type Mods = Record<string, boolean | string>
 
 export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
@@ -6,8 +5,7 @@ export function classNames(cls: string, mods: Mods = {}, additional: string[] = 
         cls,
         ...additional.filter(Boolean),
         ...Object.entries(mods)
-        .filter(([className, value]) => Boolean(value))
-        .map(([className, value]) => className)
-    ].join(" ")
+            .filter(([className, value]) => Boolean(value))
+            .map(([className, value]) => className),
+    ].join(' ');
 }
-
