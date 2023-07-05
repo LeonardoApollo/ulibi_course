e4f1c4e08a2a3d055d9ae94d27bd33ce7eb9f656
Mikhail Tyapkov <redcrownmailbox@gmail.com>
2023-07-05 23:24:37 +0300

    module5_lesson2

---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,20 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { LoginSchema } from '../types/loginSchema';
+import { loginActions, loginReducer } from './loginSlice';
+
+describe('loginSlice', () => {
+    test('test set username', () => {
+        const state: DeepPartial<LoginSchema> = { username: 'admin' };
+        expect(loginReducer(
+            state as LoginSchema,
+            loginActions.setUsername('newUser'),
+        )).toEqual({ username: 'newUser' });
+    });
+    test('test set password', () => {
+        const state: DeepPartial<LoginSchema> = { password: '123' };
+        expect(loginReducer(
+            state as LoginSchema,
+            loginActions.setPassword('12345'),
+        )).toEqual({ password: '12345' });
+    });
+});
