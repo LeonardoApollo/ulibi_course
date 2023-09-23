90d997d43d413592254d478e745506372d9a1080
Mikhail Tyapkov <redcrownmailbox@gmail.com>
2023-09-24 00:22:18 +0300

    module14_lesson8

---
+++ cypress/support/commands.ts
@@ -0,0 +1,20 @@
+import { login } from './commands/login';
+
+// // -- This is a parent command --
+Cypress.Commands.add('login', login);
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            login(username: string, password: string): Chainable<void>
+        }
+    }
+}
+
+export { };
