a257a6e8d1fc4be035d9a8ba3d34c75ab7682446
Mikhail Tyapkov <redcrownmailbox@gmail.com>
2023-10-13 00:12:10 +0300

    module15_lesson4

---
+++ scripts/remove-feature.ts
@@ -0,0 +1,77 @@
+import { Node, Project, SyntaxKind } from 'ts-morph';
+
+const removeFeatureName = process.argv[2]; // example isArticleEnabled
+const featureState = process.argv[3]; // example off/on
+
+if (!removeFeatureName) {
+    throw new Error('Feature flag was not passed');
+}
+
+if (!featureState) {
+    throw new Error('Feature flag state was not passed');
+}
+
+if (featureState !== 'on' && featureState !== 'off') {
+    throw new Error('Feature flag state must be on or off');
+}
+
+const project = new Project({});
+
+project.addSourceFilesAtPaths('src/**/*.ts');
+project.addSourceFilesAtPaths('src/**/*.tsx');
+
+const files = project.getSourceFiles();
+
+function isToggleFunction(node: Node) {
+    let isToggleFeatures = false;
+    node.forEachChild((child) => {
+        if (
+            child.isKind(SyntaxKind.Identifier) &&
+            child.getText() === 'toggleFeatures'
+        ) {
+            isToggleFeatures = true;
+        }
+    });
+    return isToggleFeatures;
+}
+
+files.forEach((sourceFile) => {
+    sourceFile.forEachDescendant((node) => {
+        if (node.isKind(SyntaxKind.CallExpression) && isToggleFunction(node)) {
+            const objectOptions = node.getFirstDescendantByKind(
+                SyntaxKind.ObjectLiteralExpression,
+            );
+
+            if (!objectOptions) return;
+
+            const onFunctionProperty = objectOptions.getProperty('on');
+            const offFunctionProperty = objectOptions.getProperty('off');
+
+            const featureNameProperty = objectOptions.getProperty('name');
+
+            const onFunction = onFunctionProperty?.getFirstDescendantByKind(
+                SyntaxKind.ArrowFunction,
+            );
+            const offFunction = offFunctionProperty?.getFirstDescendantByKind(
+                SyntaxKind.ArrowFunction,
+            );
+
+            const featureName = featureNameProperty
+                ?.getFirstDescendantByKind(SyntaxKind.StringLiteral)
+                ?.getText()
+                .slice(1, -1);
+
+            if (featureName !== removeFeatureName) return;
+
+            if (featureState === 'on') {
+                node.replaceWithText(onFunction?.getBody().getText() ?? '');
+            }
+
+            if (featureState === 'off') {
+                node.replaceWithText(offFunction?.getBody().getText() ?? '');
+            }
+        }
+    });
+});
+
+project.save();
