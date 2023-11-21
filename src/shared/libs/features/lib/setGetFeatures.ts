c686369d813992a70dd4350c7aa0aa9a4616f3af
Mikhail Tyapkov <redcrownmailbox@gmail.com>
2023-11-22 00:01:57 +0300

    module16_lesson20

--- src/shared/libs/features/lib/setGetFeatures.ts
+++ src/shared/libs/features/lib/setGetFeatures.ts
@@ -1,8 +1,15 @@
+import { LOCAL_STORAGE_DESIGN_KEY } from '@/shared/const/localestorage';
 import { FeatureFlags } from '@/shared/types/featureFlags';
+
+const defaultFeatures: FeatureFlags = {
+    isAppRedesigned: localStorage.getItem(LOCAL_STORAGE_DESIGN_KEY) === 'new',
+};
 
 // Фичи не меняются в ходе сессии, их необязательно делать реактивными
 // eslint-disable-next-line
-export let featureFlags: FeatureFlags = {};
+export let featureFlags: FeatureFlags = {
+    ...defaultFeatures,
+};
 
 export function setFeatureFlags(newFeatureFlags?: FeatureFlags) {
     if (newFeatureFlags) {
