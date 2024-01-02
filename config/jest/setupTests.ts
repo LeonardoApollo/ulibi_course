f22001321ca9e9d6412da8b67ca408109eacb513
Mikhail Tyapkov <redcrownmailbox@gmail.com>
2024-01-03 00:25:53 +0300

    redefining setImmediate for tests

--- config/jest/setupTests.ts
+++ config/jest/setupTests.ts
@@ -3,6 +3,10 @@ import '@testing-library/jest-dom';
 import 'regenerator-runtime/runtime';
 import { TextDecoder, TextEncoder } from 'util';
 
+// @ts-ignore
+global.setImmediate =
+    global.setImmediate || ((fn, ...args) => global.setTimeout(fn, 0, ...args));
+
 Object.assign(global, { TextDecoder, TextEncoder });
 Object.assign(process.env, {
     API_KEY: 'fgtshydhgtdthdtgh',
